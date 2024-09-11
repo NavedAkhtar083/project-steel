bde5ecdd2a0f111553fccaeb89e4ca934e605a34
NavedAkhtar083 <navedkhan9410671292@gmail.com>
2024-09-11 15:32:55 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+// Next/previous controls
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
+
+function prevSlide() {
+    showSlides(slideIndex -= 1);
+}
+
+function showSlides(n) {
+    const slides = document.getElementsByClassName("slide");
+    
+    if (n >= slides.length) { 
+        slideIndex = 0;
+    } 
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    
+    slides[slideIndex].style.display = "block";
+}
+
+// Auto slide (optional)
+setInterval(() => {
+    nextSlide();
+}, 3000);
